e92dd0e5e572ddd1882d697b652f7c52f5c55ecc
Emil Soman <emil.soman@gmail.com>
2016-09-02 23:44:50 +0530

    Remove hot reloading code

--- secret_question/lib/secret_question.ex
+++ secret_question/lib/secret_question.ex
@@ -1,9 +1,8 @@
 defmodule SecretQuestion do
   use GenStateMachine
 
-  @vsn 1
-
-  def start(data = {question, expected_answer} \\ {"Hello?", "world"}) when is_binary(question) and is_binary(expected_answer) do
+  def start(data = {question, expected_answer} \\ {"Hello?", "world"})
+    when is_binary(question) and is_binary(expected_answer) do
     GenStateMachine.start_link(__MODULE__, data, name: __MODULE__)
   end
 
@@ -26,15 +25,6 @@ defmodule SecretQuestion do
     end
   end
 
-  # Use this to update the state machine with new code and state
-  def upgrade do
-    Code.load_file __ENV__.file
-    :sys.suspend __MODULE__
-    :sys.change_code __MODULE__, __MODULE__, (@vsn - 1), []
-    :sys.resume __MODULE__
-  end
-
-
   # Callbacks
 
   # This gets called when the state machine starts
@@ -44,7 +34,7 @@ defmodule SecretQuestion do
   end
 
   # Handles get_question call. Returns the current question in the state machine
-  def handle_event({:call, from}, :get_question, :locked, data = {question, _}) do
+  def handle_event({:call, from}, :get_question, :locked, {question, _}) do
     {:keep_state_and_data, {:reply, from, question}}
   end
 
@@ -60,7 +50,7 @@ defmodule SecretQuestion do
   # The purpose of the ref is to ensure only the person who unlocked
   # can set the next question and answer, since this ref will be
   # checked when the lock event comes in.
-  def handle_event({:call, from}, {:unlock, answer}, :locked, data = {question, answer}) do
+  def handle_event({:call, from}, {:unlock, answer}, :locked, {_question, answer}) do
     ref = make_ref
     {:next_state, :unlocked, ref, {:reply, from, {:unlocked, ref}}}
   end
@@ -74,7 +64,8 @@ defmodule SecretQuestion do
   # Handles the lock event. This will check if the event is coming from
   # the person who has periviously unlocked it by matching the ref.
   # If the refs are matching, locking happens by setting a new question and answer
-  def handle_event(:cast, {:lock, ref, data = {question, answer}}, :unlocked, ref)  when is_binary(question) and is_binary(answer) do
+  def handle_event(:cast, {:lock, ref, data = {question, answer}}, :unlocked, ref)
+    when is_binary(question) and is_binary(answer) do
     {:next_state, :locked, data}
   end
 end
